90ff0db25a3fe2144663ea11242eb6d086cee356
Lucio Anastasia <lucioana@mi-f1.msk.21-school.ru>
2022-07-07 21:12:08 +0300

    quest 3

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
-int sum(int a, int b);
+int sum(int a, int b) { return a + b; }
 
-int main()
-{
-    return 0;
-}
+int diff(int a, int b) { return a - b; }
 
-int sum(int a, int b){
-    return (2 * a * b) / (a - b);
+int mult(int a, int b) { return a * b; }
+
+int div(int a, int b) { return a / b; }
+
+int main() {
+    int a, b;
+    if (scanf("%d %d", &a, &b) == 2) {
+        if (b == 0)
+            printf("%d, %d, %d, n/a\n", sum(a, b), diff(a, b), mult(a, b));
+        else
+            printf("%d %d %d %d\n", sum(a, b), diff(a, b), mult(a, b), div(a, b));
+    } else {
+        printf("n/a\n");
+    }
 }
